727fab6fa9d72062ea688c06f23fc6f9f2f96640
Alejandro <jabazan@gmail.com>
2021-09-27 11:46:00 -0300

    Agregado de JQuery para efectos de fadeIn

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-//Get the button:
+
+
+
+//Boton ir a top
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 0px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     mybutton.style.display = "block";
 } else {
     mybutton.style.display = "none";
 }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+
 function topFunction() {
 document.body.scrollTop = 0; // For Safari
 document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
