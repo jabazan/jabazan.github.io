8254c862c61f82ee59394e6797efdeb6030a1e7b
jabazan <jabazan@gmail.com>
2022-01-22 20:42:15 -0300

    Cambios varios

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Funcion para ir hacia la parte superior de la pagina
 function topFunction() {
-  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
@@ -25,19 +25,5 @@ function cambioAExperiencia() {
       document.getElementById("experiencia").style.display = "none";
       document.getElementById("formacion").style.display = "block";
     }
-  }
-}
-
-//Boton ir a top
-mybutton = document.getElementById("myBtn");
-
-window.onscroll = function () {
-  scrollFunction();
-};
-function scrollFunction() {
-  if (document.body.scrollTop > 0) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
   }
 }
